59b34e8f7d09450c64b19b0075ad2b02ca148d55
Sanja Mandic <sanja1mandic@gmail.com>
2022-05-18 10:59:10 +0200

    fix linters error

--- script.js
+++ script.js
@@ -154,25 +154,20 @@ for (let i = 0; i < 6; i += 1) {
     speakerClone.style.display = 'none';
     speakersContainer.appendChild(moreBtn);
     speakersContainer.style.gridTemplateColumns = 'repeat(1,auto)';
-  }
-  else if (i >= 2) {
+  } else if (i >= 2) {
     speakersContainer.appendChild(moreBtn);
     moreBtn.style.display = 'none';
     speakersContainer.style.gridTemplateColumns = 'repeat(2,auto)';
-  }
-  else {
+  } else {
     speakersContainer.style.gridTemplateColumns = 'repeat(2,auto)';
   }
 
   window.addEventListener('resize', () => {
-
     if (i >= 2 && window.screen.width <= 768) {
       speakerClone.style.display = 'none';
       moreBtn.style.display = 'flex';
       speakersContainer.style.gridTemplateColumns = 'repeat(1,auto)';
-    }
-
-    else {
+    } else {
       speakerClone.style.display = 'flex';
       moreBtn.style.display = 'none';
       lessBtn.style.display = 'none';
